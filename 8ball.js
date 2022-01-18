a6eb4cd1b449786aadf9dc3d943484aaaade5123
Senphire <91835491+Senphire@users.noreply.github.com>
2022-01-18 21:24:44 +0100

    Add files via upload

---
+++ 8ball.js
@@ -0,0 +1,15 @@
+@$(user), the 8-ball says... 
+$(eval a=["sure", "are you kidding?!", "yeah", "no", "I think so", "don't bet on it", "ja", "nee", 
+"doubtful", "for sure", "forget about it", "nein", "maybe", "Kappa Keepo PogChamp", "sure", 
+"I don't think so", "it is so", "leaning towards no", 
+"look deep in your heart and you will see the answer", "most definitely", "most likely", 
+"my sources say yes", "never", "nah m8", "might actually be yes", "no.", "outlook good", 
+"outlook not so good", "perhaps", "mayhaps", "that's a tough one", "idk kev", "don't ask that", 
+"the answer to that isn't pretty", "the heavens point to yes", "who knows?", "without a doubt", 
+"yesterday it would've been a yes, but today it's a yep", "you will have to wait"]; 
+a[Math.floor(Math.random()*a.length)];) 
+$(eval b=["Kappa", "Keepo", "xD", "KKona", "4Head", "EleGiggle", "DansGame", "KappaCool", "BrokeBack", 
+"OpieOP", "KappaRoss", "KappaPride", "FeelsBadMan", "FeelsGoodMan", "FeelsBirthdayMan", "FeelsAmazingMan", 
+"PogChamp", "VisLaud", "OhMyDog", "FrankerZ", "DatSheffy", "BabyRage", "VoHiYo", "haHAA", "LUL", 
+"ResidentSleeper", "SourPls", "TriHard", "SMOrc"]; 
+b[Math.floor(Math.random()*b.length)])
